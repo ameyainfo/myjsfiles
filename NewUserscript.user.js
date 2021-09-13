f7c6412a97621b535da1c789fb402055d4e81892
satishganni <70859249+satishganni@users.noreply.github.com>
2021-09-14 01:24:20 +0530

    Update NewUserscript.user.js

--- NewUserscript.user.js
+++ NewUserscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Isha PRS Script
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.0
 // @description  try to take over the world!
 // @author       You
 // @match        https://prs.innerengineering.com/ieo/newadmin/iecsoAdminMgmt.php
