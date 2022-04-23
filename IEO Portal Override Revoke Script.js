506dd0575b74e80f48396d103f6b6f7fa0c08b92
ameyainfo <72103674+ameyainfo@users.noreply.github.com>
2022-04-23 10:38:18 +0530

    Update IEO Portal Override Revoke Script.js

--- IEO Portal Override Revoke Script.js
+++ IEO Portal Override Revoke Script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         New IEO Portal Script
+// @name         IEO Portal Override Revoke Script
 // @namespace    http://tampermonkey.net/
-// @version      4.21
+// @version      4.22
 // @description  this is IEO New Admin script
 // @author       You
 // @match        https://prs-admin.innerengineering.com/?kdr=eyJyb3V0ZSI6IkFwcC9NYWluL2llY29zdXBwb3J0IiwiYWN0aW9uIjoiaW5kZXgifQ==
