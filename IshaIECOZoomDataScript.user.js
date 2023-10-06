b2e726ef7641614fd10b82b12868068a257179fd
ChellaIECO <97609375+ChellaIECO@users.noreply.github.com>
2023-10-07 00:33:41 +0530

    Update IshaIECOZoomDataScript.user.js

--- IshaIECOZoomDataScript.user.js
+++ IshaIECOZoomDataScript.user.js
@@ -1,35 +1,55 @@
 // ==UserScript==
-// @name         Isha PRS Zoom Script
+// @name         Sanity Script 2023 New IEO
 // @namespace    http://tampermonkey.net/
-// @version      1.5
-// @description  this is IEO Admin script (zoom link)
+// @version      1.0
+// @description  try to take over the world!
 // @author       You
-// @match        https://prs.innerengineering.com/ieo/newadmin/iecsoAdminMgmt.php
-// @icon         https://www.google.com/s2/favicons?domain=google.com
+// @match        https://prs-admin.innerengineering.com/?kdr=eyJyb3V0ZSI6IkFwcC9QUlNNYW5hZ2VtZW50L2llb3N1cHBvcnQiLCJhY3Rpb24iOiJpbmRleCIsInBhcmFtcyI6bnVsbH0=
+// @match        https://prs-admin.innerengineering.com/index?kdr=eyJyb3V0ZSI6IkFwcC9QUlNNYW5hZ2VtZW50L2llb3N1cHBvcnQiLCJhY3Rpb24iOiJpbmRleCIsInBhcmFtcyI6bnVsbH0=
 // @grant        GM_setClipboard
 // @require https://code.jquery.com/jquery-1.8.3.min.js
+// @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
 // ==/UserScript==
-
+/* globals jQuery, $, waitForKeyElements */
 var msg = '';
+waitForKeyElements (".table-striped", myFunc);
 
+var myInt;
 (function() {
     'use strict';
 
-    document.getElementById('searchType0').checked = true;
-    document.getElementById('email').focus();
+    myInt = setInterval(myFunc, 1000);
 
-    if(document.getElementById('iecsouser').style.display == '' || document.getElementById('iecsouser').style.display == 'block')
-    {
-        msg += $('#iecsouser .dl-horizontal dd')[1].innerHTML.replace('&nbsp;', '') + ', ';
-        msg += $('#iecsouser .dl-horizontal dd')[2].innerHTML.replace('&nbsp;', '') + ', ';
-        msg += $('#iecsouser .dl-horizontal dd')[4].innerHTML.replace('&nbsp;', '') + ', ';
-        msg += $('#iecsouser .dl-horizontal dd')[5].innerHTML.replace('&nbsp;', '') + ', ';
-
-        msg += $('#sample_2 tr:last td:nth-child(6)').text() + ',';
-
-        GM_setClipboard (msg);
-        alert('Message copied!!!');
-    }
+    // Your code here...
 })();
 
-
+function myFunc(){
+    let text = $('table thead tr:first-child th:nth-child(1)').html().trim();
+    let SessionYes = text.includes("Session");
+    alert(SessionYes)
+    let RollNumberYes = text.includes("Roll No. | Reg Id");
+    if(RollNumberYes || SessionYes)
+    {
+        clearInterval(myInt);
+    }
+    else
+    {
+        return;
+    }
+    if(SessionYes)
+    {
+    alert($( "table tbody tr" ).length);
+    return;
+    }
+    msg = "";
+    var test = $('.card-header:contains("User Course Progress")').parent().children('.card-body:contains("Current Step")').html();
+    var CurrentStep = test.slice(test.indexOf('Current Step') + 44, test.indexOf('Current Step')+45);
+    var language = test.slice(test.indexOf('Language') + 40, test.indexOf('Language')+42);
+    var classDetail = $('table tbody td:nth-child(3)').html().split('<br>')[0];
+    var progDetail = classDetail.trim();
+    msg += '="Initiation:"&CHAR(10)&"' + progDetail + '"'
+    msg += '&CHAR(10)&"Current Step: ' + CurrentStep + '"'
+    GM_setClipboard (msg);
+    alert('Message copied!!!');
+    return;
+    }
